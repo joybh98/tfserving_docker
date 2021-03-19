0d9f73e1d3596d010d6d3967da8c8145906672d5
Joy Bhalla <joybhalla9@gmail.com>
2021-03-19 21:23:21 +0530

    added np.ndarray for test

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-RUN docker pull tensorflow/serving
-
-RUN docker run -t --rm -p 8501:8501 -v "/home/joy/models/nlp_model:/models/nlp_model" -e MODEL_NAME=nlp_model tensorflow/serving &
--- requests_tf.py
+++ requests_tf.py
@@ -1,20 +1,16 @@
-# sending requests and getting outputs
-
-import requests
 import base64
+import requests
+import numpy as np
 import json
 
+SERVER_URL='http://localhost:8501/v1/models/nlp_model:predict'
 headers={"content-type":"application/json"}
-SERVER_URL= 'http://localhost:8501/v1/models/nlp_model:predict'
 
-loaded_text=open('example_sentence.txt','rb').read()
-txt_bytes = base64.b64encode(loaded_text).decode('utf-8')
+test_data = np.ndarray(shape=(100,)).tolist()
 
 body = {
     "signature_name": "serving_default",
-    "examples" : [{
-        "x": { "b64": txt_bytes},  
-     }]
+    "examples": test_data
 }
 
 r = requests.post(SERVER_URL,data=json.dumps(body),headers=headers)
