bf4db728fa9e6ecd77c34179178efcd0469c45a8
Joy Bhalla <joybhalla9@gmail.com>
2021-03-20 15:16:41 +0530

    changed structure of data body

--- requests_tf.py
+++ requests_tf.py
@@ -3,16 +3,20 @@ import requests
 import numpy as np
 import json
 
-SERVER_URL='http://localhost:8501/v1/models/nlp_model:regress'
+SERVER_URL='http://localhost:8501/v1/models/nlp_model:predict'
 headers={"content-type":"application/json"}
 
 test_data = np.ndarray(shape=(100,),dtype=int).tolist()
 
 body = {
-    "instances": test_data
+    "instances":[
+        {"txt_example":test_data}
+    ]
 }
 
 # r will show the http response ex:<Response [400]>, <Response [200]> etc
-r = requests.post(SERVER_URL,data=json.dumps(body),headers=headers)
-
-print(json.loads(r.text))
+try:
+    r = requests.post(SERVER_URL,data=json.dumps(body),headers=headers)
+    print(json.loads(r.text))
+except ConnectionError:
+    print('Cant connect!')
